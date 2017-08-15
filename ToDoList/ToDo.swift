d89c8c22a527a95b37d4197950dd4337eb2baf55
Regina Imhoff <regina.imhoff@gmail.com>
2017-08-15 12:16:01 -0500

    added class for ToDo list

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Regina Imhoff on 8/15/17.
+//  Copyright © 2017 Regina Imhoff. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+
+}
