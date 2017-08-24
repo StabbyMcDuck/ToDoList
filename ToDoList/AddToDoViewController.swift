b85e9b95db0b2c42e8bc269227e94c8ddd797b14
Regina Imhoff <regina.imhoff@gmail.com>
2017-08-24 10:09:33 -0500

    fixed optionals

--- ToDoList/AddToDoViewController.swift
+++ ToDoList/AddToDoViewController.swift
@@ -17,36 +17,23 @@ class AddToDoViewController: UIViewController {
     @IBAction func addTapped(_ sender: Any) {
         let toDo = ToDo()
         
-        toDo.name = titleTextField.text!
-        toDo.important = importantSwitch.isOn
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+            
+            previousVC.toDos.append(toDo)
+            previousVC.tableView.reloadData()
+            
+            navigationController?.popViewController(animated: true)
+        }
         
-        previousVC.toDos.append(toDo)
-        
-        previousVC.tableView.reloadData()
-        
-        navigationController?.popViewController(animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
